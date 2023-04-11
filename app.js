aca2469706f822bfb5bbb3703c1b2a39d600d93e
Jakob <shavinski.jakob@gmail.com>
2023-04-11 16:03:35 -0700

    finish html page

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Let's get this party started!");
+
+const $submitButton = $('.submit-button');
+const $deleteButton = $('.delete-button');
+
+
+$submitButton.on('click', function (e) {
+    e.preventDefault();
+    const $userInput = $('input').val();
+
+    let userGif = axios.post();
+
+    console.log($userInput);
+});
